c1b02c2ed4030010445e501ac8a04d7a023512cc
weimo <1585558481@qq.com>
2019-12-23 17:16:10 +0800

    准备工作-基础布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+// 导入路由
+// 提供的一个绝对路径别名
+// import router from '@/router'
+import router from './router'
+
+Vue.use(ElementUI)
+// 在控制台输出日志，日志
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-login">
+    Login
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.container-login {
+    // 全屏
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: url(../../assets/login_bg.jpg) no-repeat;
+}
+</style>
